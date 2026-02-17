8017c7c7155afa73b2a1e42b7b2771e3bf8488cf
sir-ct <davidsirct@gmail.com>
2026-02-17 13:53:51 +0100

    fixing pixels

--- src/components/VideoRankingComponent/index.tsx
+++ src/components/VideoRankingComponent/index.tsx
@@ -7,14 +7,14 @@ interface VRCInterface {
 
 export default function VideoRankingComponent(props: VRCInterface){
     return(
-        <div className="text-white flex gap-5 items-center justify-between w-[90vw] lg:w-[80vw]">
+        <div className="text-white flex gap-8 items-center justify-between w-[90vw] lg:w-[80vw]">
             <div className="text-[20px] lg:text-[64px] font-bold">{props.position}</div>
             <div className="flex gap-4 items-center justify-center border-b-[1px] py-2 lg:py-6 border-[#433E3E]">
                 <img src={Images.rankingImg} alt="" className="w-[55px] h-[55px] lg:w-[128px] lg:h-[128px]" />
                 <div className="w-[60vw]">
-                    <div className="text-[15px] lg:text-[28px] line-clamp-2">
+                    <p className="text-[15px] w-[85%] lg:text-[28px] line-clamp-2">
                         Lorem ipsum dolor sit amet consectetur. Habitant odio morbi accumsan non 
-                    </div>
+                    </p>
                     <p className="text-[12px] lg:text-[27px] text-[#747474]">
                         Sydney Talker
                     </p>
