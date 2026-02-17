4369dfc91835663fae6c52d765076393be66127a
sir-ct <davidsirct@gmail.com>
2026-02-17 12:48:41 +0100

    pushing space in ranking

--- src/components/VideoRankingComponent/index.tsx
+++ src/components/VideoRankingComponent/index.tsx
@@ -7,7 +7,7 @@ interface VRCInterface {
 
 export default function VideoRankingComponent(props: VRCInterface){
     return(
-        <div className="text-white flex gap-2 lg:gap-5 items-center justify-between w-[90vw] lg:w-[80vw]">
+        <div className="text-white flex gap-5 items-center justify-between w-[90vw] lg:w-[80vw]">
             <div className="text-[20px] lg:text-[64px] font-bold">{props.position}</div>
             <div className="flex gap-4 items-center justify-center border-b-[1px] py-2 lg:py-6 border-[#433E3E]">
                 <img src={Images.rankingImg} alt="" className="w-[55px] h-[55px] lg:w-[128px] lg:h-[128px]" />
